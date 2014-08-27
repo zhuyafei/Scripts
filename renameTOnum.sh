c7063dba59946b1aecdb883d747a687acd8b4fa0
zhuyafei <zhuyafei4520@163.com>
2014-08-27 11:24:33 +0800

    Update renameTOnum.sh

--- renameTOnum.sh
+++ renameTOnum.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+if [ $# != 2 ]; then
+	echo -ne "Arguments Error.\n"
+	echo -ne "Usage:\n"
+	echo -ne "\t$0 <Dir> <Extension>\n"
+	exit 7
+else
 num=1
 dir=$1
+Extension=$2
 for i in $(find ${dir} -type f)
 do
-	mv $i $dir/${num}.png
+	mv $i $dir/${num}.${Extension}
 	((num++))
 done
-
+fi
 
