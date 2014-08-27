c22228053da03391b64430bedbc37d6ffbebd371
zhuyafei <zhuyafei4520@163.com>
2014-08-27 14:06:45 +0800

    Modify the extension of files

--- renameTOnum.sh
+++ renameTOnum.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-if [ $# != 2 ]; then
+if [ $# != 1 ]; then
 	echo -ne "Arguments Error.\n"
 	echo -ne "Usage:\n"
-	echo -ne "\t$0 <Dir> <Extension>\n"
+	echo -ne "\t$0 <Dir>\n"
 	exit 7
 else
 num=1
 dir=$1
-Extension=$2
 for i in $(find ${dir} -type f)
 do
+	Extension=${i##*.}
 	mv $i $dir/${num}.${Extension}
 	((num++))
 done
